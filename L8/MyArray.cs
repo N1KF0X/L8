acc144ccb2c4bb1880cc7c8f11ca8a7b5d8cce2f
N1KF0X <nickit.baickin@yandex.ru>
2023-03-19 13:08:15 +0300

    Добавьте файлы проекта.

---
+++ L8/MyArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8
+{
+    internal class MyArray <T>
+    {
+        public T[] array;
+
+        public MyArray(T[] array)
+        {
+            this.array = array; 
+        }
+
+        public void AddElement(T item, int index)
+        {
+            if (index >= 0 || index < array.Length)
+            {
+                this.array[index] = item;
+            }
+            else
+            {
+                Console.WriteLine($"Индекс выходит за пределы массива. Максимальный индекс: {array.Length - 1}");
+            }
+        }
+
+        public void RemoveElement(int index) 
+        {
+            if (index >= 0 || index < array.Length)
+            {
+                array[index] = default;
+            }
+            else
+            {
+                Console.WriteLine($"Индекс выходит за пределы массива. Максимальный индекс: {array.Length - 1}");
+            }
+        }
+
+        public T GetElement(int index)
+        {
+            T element = default;
+            if (index >= 0 || index < array.Length)
+            {
+                element = array[index];
+            }
+            else
+            {
+                Console.WriteLine($"Индекс выходит за пределы массива. Максимальный индекс: {array.Length - 1}");             
+            }
+            return element;
+        }
+
+        public int GetLength()
+        {
+            return array.Length;
+        }
+    }
+}
---
+++ L8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[5];
+            string[] strArray = new string[3];
+            decimal[] decArray = new decimal[4];
+            MyArray<int> IntMyArray = new MyArray<int>(intArray);
+            MyArray<string> StrMyArray = new MyArray<string>(strArray);
+            MyArray<decimal> DecMyArray = new MyArray<decimal>(decArray);
+
+            IntMyArray.AddElement(4, 1);
+            Console.WriteLine(IntMyArray.array[1]);
+            Console.WriteLine(IntMyArray.GetElement(1));
+            IntMyArray.RemoveElement(1);
+            Console.WriteLine(IntMyArray.GetElement(1));
+            Console.WriteLine(IntMyArray.GetLength());           
+        }
+    }
+}
